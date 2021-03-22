75b6b7e2ac5bfc39816138f7d23ad40bd2af909e
Tomerfi1210 <tomerfi1210@gmail.com>
2021-03-22 13:49:51 +0200

    add creation of db

--- database.py
+++ database.py
@@ -1,16 +1,26 @@
 import psycopg2
+from psycopg2 import sql
 from configparser import ConfigParser
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
 class Database:
     """class the responsible for the database"""
     def __init__(self, file_path: str):
-        self.__connection = self.__connect_db(file_path)
+        self.__connect_db(file_path)
         if self.__connection:
             self.__create_table()
 
     def __del__(self):
         self.__connection.close() # close the connection
+    def __db_exists(self, db_name):
+        cur = self.__connection.cursor()
+        cur.execute("SELECT datname FROM pg_database")
+        db_list = cur.fetchall()
+        self.__connection.commit()
+        if(db_name,) in db_list:
+            return True
+        return False
 
     def __connect_db(self, file_path: str):
         """
@@ -25,15 +35,30 @@ class Database:
 
         try:
             # read values from a section
-            connection = psycopg2.connect(user=config.get('postgresql', 'user'),
+            # conect to defualt db
+            self.__connection = psycopg2.connect(user=config.get('postgresql', 'user'),
                                           password=config.get('postgresql', "password"),
                                           host=config.get('postgresql', "host"),
-                                          port=config.get('postgresql', "port"),
-                                          database=config.get('postgresql', "database"))
+                                          port=config.get('postgresql', "port"))
+            self.__connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+            if not self.__db_exists(config.get('postgresql', 'database')):
+                self.__create_db(config.get('postgresql', 'database'))
+            self.__connection.close()
+            self.__connection = psycopg2.connect(user=config.get('postgresql', 'user'),
+                                         password=config.get('postgresql', "password"),
+                                         host=config.get('postgresql', "host"),
+                                         port=config.get('postgresql', "port"),
+                                         database=config.get('postgresql', 'database'))
+
         except Exception as e:
-            print('cannot access data base')
+            print(e)
             return None
-        return connection
+
+    def __create_db(self, db_name):
+        curs = self.__connection.cursor()
+        # command =  + db_name+";"
+        curs.execute(sql.SQL("CREATE DATABASE {}".format(db_name)))
+
 
     def __create_table(self):
         """function that create table in database"""
